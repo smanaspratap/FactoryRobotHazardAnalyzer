ce69fe3a688f93beb1915e5f558761d67eae5454
smanaspratap <manaspratap2003@gmail.com>
2026-02-14 11:25:04 +0530

    UC2: Accept robot hazard inputs and echo them

--- FactoryRobotHazardAnalyzer.java
+++ FactoryRobotHazardAnalyzer.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
         System.out.println("Factory Robot Hazard Analyzer");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Arm Precision (0.0 - 1.0):");
+        double armPrecision = sc.nextDouble();
+
+        System.out.println("Enter Worker Density (1 - 20):");
+        int workerDensity = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
+        String machineryState = sc.nextLine();
+
+        // Echo inputs back to the user (UC2 requirement)
+        System.out.println("Arm Precision: " + armPrecision);
+        System.out.println("Worker Density: " + workerDensity);
+        System.out.println("Machinery State: " + machineryState);
+
+        sc.close();
     }
 }
